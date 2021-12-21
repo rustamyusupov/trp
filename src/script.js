063db37e146302278cd9e43066e24dc5b7be09da
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-12-21 23:40:50 +0200

    render hint

--- src/script.js
+++ src/script.js
@@ -1,21 +1,60 @@
-const trUrl = '/cycling/workouts';
+const urls = {
+  trWorkouts: 'trainerroad.com/app/cycling/workouts',
+  tpCalendar: 'trainingpeaks.com/#calendar',
+};
+
+const hints = {
+  tp: 'No saved workouts.<br />Please open TR&nbsp;workout.',
+  other: 'Please open TR&nbsp;workout or TP&nbsp;calendar.',
+};
 
 const getUrl = async () => {
   const option = { active: true, currentWindow: true };
   const [tab] = await chrome.tabs.query(option);
 
-  return tab?.url;
+  return tab?.urls;
 };
 
-const init = async () => {
-  const url = await getUrl();
-  const isTr = url.includes(trUrl);
+const textRender = (content) => {
   const list = document.getElementById('list');
 
-  if (!isTr) {
-    list.innerHTML = 'Open TR workout';
+  list.innerHTML = content;
+};
+
+const listRender = (content) => {
+  const list = document.getElementById('list');
+
+  list.innerHTML = 'Workouts';
+};
+
+const formatters = {
+  text: textRender,
+  list: listRender,
+};
+
+const render = (data, format) => formatters[format](data);
+
+const init = async () => {
+  const pageUrl = await getUrl();
+  const isWorkouts = pageUrl.includes(urls.trWorkouts);
+  const isCalendar = pageUrl.includes(urls.tpCalendar);
+  const isTargetPage = isWorkouts || isCalendar;
+
+  if (!isTargetPage) {
+    render(hints.other, 'text');
     return;
   }
+
+  // get saved workouts
+  const workouts = [];
+  const hasWorkoutsForTp = isCalendar && workouts.length === 0;
+
+  if (!hasWorkoutsForTp) {
+    render(hints.tp, 'text');
+    return;
+  }
+
+  render(workouts, 'list');
 };
 
 init();
